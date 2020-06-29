f9f3d158d890a9128eeadec3914732a2c4b9db42
Andrew Tran <andrewtttran@gmail.com>
2020-06-30 09:58:40 +1000

    unfinished hooks refactor

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,39 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 import Number from './Number';
 
+
+// function App() {
+//   const[number, setNumber] = useState('')
+//   const[option, setOption] = useState('')
+  
+//   function setNumber() {
+
+//   }
+
+// // first thing is the state, second is the function that allows you to update the state
+
+//     onSearchSubmit = async (numberSel, option) => {
+//     const response = await axios.get(
+//       `http://numbersapi.com/${numberSel}/${option}`
+//     );
+
+//     this.setState({ number: response.data });
+//   };
+
+//   render() {
+//     return (
+//       <div className="ui container" style={{ marginTop: '10px' }}>
+//         <SearchBar onSubmit={this.onSearchSubmit} />
+//         <Number number={this.state.number} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+///////////////////////////////////////////////////////
+
 class App extends React.Component {
   state = { number: '' };
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,73 @@
 import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 
+
+function SearchBar () {
+  const [number, setNumber] = useState('')
+
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (event.keyCode === 13) {
+      this.props.onSubmit(this.state.numberSel, this.state.option);
+    }
+  };
+
+  render() {
+    const numberOptions = [
+      {
+        key: 'trivia',
+        text: 'Trivia',
+        value: 'trivia',
+      },
+      {
+        key: 'year',
+        text: 'Year',
+        value: 'year',
+      },
+      {
+        key: 'date',
+        text: 'Date',
+        value: 'date',
+      },
+      {
+        key: 'math',
+        text: 'Math',
+        value: 'math',
+      },
+    ];
+    return (
+      <div className="ui segment">
+        <div className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Search a number</label>
+            <Dropdown
+              placeholder="Select"
+              fluid
+              selection
+              options={numberOptions}
+              onChange={(e) =>
+                this.setState({ option: e.target.innerText.toLowerCase() })
+              }
+            />
+            <input
+              type="text"
+              value={this.state.numberSel}
+              onChange={(e) => this.setState({ numberSel: e.target.value })}
+              onKeyUp={this.onFormSubmit}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
+
+
+//////////////////////////////////////////////////////
 class SearchBar extends React.Component {
   state = { numberSel: '', option: '' };
 
